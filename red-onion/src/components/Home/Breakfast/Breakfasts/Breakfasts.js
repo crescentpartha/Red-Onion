f4ca83fa1ee4a7ef2a606671b77cfb7c0d41a3af
crescentpartha <crescentpartha406@gmail.com>
2022-09-04 01:15:31 +0600

    Getting an error when try to load data using fetch | Uncaught (in promise) SyntaxError: Unexpected token <, DOCTYPE ... is not valid JSON

--- red-onion/src/components/Home/Breakfast/Breakfasts/Breakfasts.js
+++ red-onion/src/components/Home/Breakfast/Breakfasts/Breakfasts.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useBreakfast from '../../../../hooks/useBreakfast';
+import Breakfast from '../Breakfast/Breakfast';
 
 const Breakfasts = () => {
+    const [breakfasts] = useBreakfast();
+    console.log(breakfasts);
     return (
         <div>
-            <p className='fw-semibold'>Breakfasts</p>
+            <p className='fw-semibold'>Breakfasts: <span>{breakfasts.length}</span></p>
+            <div>
+                {
+                    breakfasts.map(breakfast => <Breakfast
+                        key={breakfast.id}
+                        breakfast={breakfast}
+                    ></Breakfast>)
+                }
+            </div>
         </div>
     );
 };
